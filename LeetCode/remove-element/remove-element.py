0664f840f01df0a9a9df7f95844ade0882ea0374
J <68155263+jxxxxe@users.noreply.github.com>
2023-05-21 23:59:26 +0900

    Time: 47 ms (33.37%) | Memory: 16.2 MB (44.40%) - LeetSync

---
+++ LeetCode/remove-element/remove-element.py
@@ -0,0 +1,11 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        index=0
+        for i,n in enumerate(nums):
+            if n==val:
+                index+=1
+                nums[i]=0
+                continue
+            nums[i-index]=n
+            
+        return len(nums)-index
