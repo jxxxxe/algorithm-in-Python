951dce84d8b002fa9a6cdedda8da259a762db400
J <68155263+jxxxxe@users.noreply.github.com>
2023-12-18 14:40:46 +0900

    [Silver III] Title: 타노스, Time: 44 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.py
+++ 백준/Silver/20310. 타노스/타노스.py
@@ -1,9 +1,15 @@
-from collections import Counter
-
 S=input()
+answer=""
 
-counts = Counter(S)
+count0,count1= S.count('0')//2,S.count('1')//2
 
-count0,count1= counts['0']//2,counts['1']//2
+for s in S:
+    if s == '0' and count0>0:
+        answer+='0'
+        count0-=1
+    elif s == '1' and count1>0:
+        count1-=1
+    elif s=='1' and count1==0:
+        answer+='1'
 
-print("0"*count0+"1"*count1)
+print(answer)
