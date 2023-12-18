cb9dabae2cda2bcfcb3fbf94b8dee2ae7423b209
J <68155263+jxxxxe@users.noreply.github.com>
2023-12-18 12:15:31 +0900

    [Silver I] Title: 미로 탐색, Time: 76 ms, Memory: 34044 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,26 +1,23 @@
 import sys
 from collections import deque
 
-def next_append(x,y):
-    if x>=0 and y>=0 and x<N and y<M and miro[x][y]:
-        miro[x][y] = 0
-        bfs.append([x, y])
-
 N, M = map(int, sys.stdin.readline().split())
+maze=[[int(j) for j in sys.stdin.readline().strip()] for _ in range(N)]
+queue=deque([[0,0]])
 
-miro = [list(map(int, sys.stdin.readline().strip())) for _ in range(N)]
-answer = 0
-bfs = deque([[0,0]])
+def bfs():
+    count = 1
+    while len(queue)>0:
+        for _ in range(len(queue)):
+            [x,y] = queue.popleft()
+            if x==N-1 and y==M-1:
+                return count
+            if x>=0 and y>=0 and x<N and y<M and maze[x][y]==1:
+                queue.append([x+1,y])
+                queue.append([x,y+1])
+                queue.append([x-1,y])
+                queue.append([x,y-1])
+                maze[x][y]=2
+        count+=1
 
-while True:
-    answer += 1
-    size = len(bfs)
-    for _ in range(size):
-        cur_x, cur_y = bfs.popleft()
-        if cur_x == N-1 and cur_y == M-1:
-            print(answer)
-            exit()
-        next_append(cur_x+1, cur_y)
-        next_append(cur_x, cur_y+1)
-        next_append(cur_x-1, cur_y)
-        next_append(cur_x, cur_y-1)
+print(bfs())
