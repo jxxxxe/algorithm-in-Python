777f3e4f600877894f59f3a8ad024bec972e12da
J <68155263+jxxxxe@users.noreply.github.com>
2023-12-19 12:12:30 +0900

    [Silver III] Title: 바이러스, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -0,0 +1,20 @@
+N=int(input())
+pair_count=int(input())
+graph=[set() for _ in range(N)]
+answer=set([0])
+
+for _ in range(pair_count):
+    a, b = map(int, input().split())
+    graph[a-1].add(b-1)
+    graph[b-1].add(a-1)
+
+def dfs(idx):
+    if not graph[idx]:
+        return
+    for computer in graph[idx]:
+        if computer not in answer:
+            answer.add(computer)
+            dfs(computer)
+
+dfs(0)
+print(len(answer)-1)
