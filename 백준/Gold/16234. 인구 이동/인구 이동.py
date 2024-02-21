53ce52741e9947271fe8cb099847f4a90118b5ad
J <68155263+jxxxxe@users.noreply.github.com>
2024-02-22 02:03:16 +0900

    [Gold IV] Title: 인구 이동, Time: 5848 ms, Memory: 34244 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.py
+++ 백준/Gold/16234. 인구 이동/인구 이동.py
@@ -1,7 +1,7 @@
 import sys
 from collections import deque
-
 sys.setrecursionlimit(10**6)
+
 
 input = sys.stdin.readline
 
@@ -14,13 +14,20 @@ answer=0
 while True:
     mark=[[0 for _ in range(n)] for _ in range(n)]
     num=0
+    dic={}
 
     def dfs(i,j,num):
         global mark
+        global dic
         mark[i][j]=num
+        if num not in dic:
+            dic[num]=[0,0]
+        dic[num][0]+=graph[i][j]
+        dic[num][1]+=1
+
         for p in pos:
             ni, nj = i+p[0], j+p[1]
-            if 0<=ni<n and 0<=nj<n and mark[ni][nj]==0 and  l<=abs(graph[i][j]-graph[ni][nj])<=r:
+            if 0<=ni<n and 0<=nj<n and mark[ni][nj]==0 and l<=abs(graph[i][j]-graph[ni][nj])<=r:
                 dfs(ni,nj,num)
 
     for i in range(n):
@@ -31,17 +38,8 @@ while True:
 
     if num==n*n:
         break
-    
+
     answer+=1
-    dic={}
-    for i in range(n):
-        for j in range(n):
-            idx=mark[i][j]
-            if mark[i][j] not in dic:
-                dic[idx]=[0,0]
-            if mark[i][j]:
-                dic[idx][0]+=graph[i][j]
-                dic[idx][1]+=1
     
     for i in range(n):
         for j in range(n):
