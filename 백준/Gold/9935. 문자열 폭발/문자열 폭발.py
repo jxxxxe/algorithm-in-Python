f37680c440a9af1f0ba078b7753df415049f4122
J <68155263+jxxxxe@users.noreply.github.com>
2024-04-10 06:53:14 +0900

    [Gold IV] Title: 문자열 폭발, Time: 452 ms, Memory: 42300 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -5,7 +5,7 @@ len_target=len(target)
 stack=[]
 for s in string:
     stack.append(s)
-    while len(stack)>=len_target and stack[-1]==target[-1] and ''.join(stack[-len_target:])==target:
+    if s==target[-1] and stack[-len_target:]==list(target):
         for _ in range(len_target):
             stack.pop()
 
